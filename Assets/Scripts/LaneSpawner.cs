f93bbadbab037fdc4d7cd6e4012f81d98076232d
Brian Nguyen <briannguyen33@gmail.com>
2016-09-18 17:09:35 -0700

    first commit procedural lane generation added

---
+++ Assets/Scripts/LaneSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaneSpawner : MonoBehaviour {
+
+
+	public GameObject[] lanes;
+	public float spawnHorizon = 500f;
+	private float offsetNextLane = 0; 
+	public float laneWidth = 2; 
+	public Transform spawnParent;
+	public GameObject player; 
+
+	void Start () {
+
+	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float positionForward = player.transform.position.z;
+
+		if (positionForward + spawnHorizon > offsetNextLane) {
+
+
+			int laneIndex = Random.Range (0, lanes.Length);
+			GameObject lane = lanes [laneIndex];
+			Vector3 nextLanePosition = offsetNextLane * Vector3.forward;
+			GameObject newLane = Instantiate (lane, nextLanePosition, Quaternion.identity) as GameObject;
+			newLane.transform.parent = spawnParent;
+			offsetNextLane += laneWidth;
+
+
+		}
+
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	CardboardHead cardhead; 
+	private Rigidbody rb; 
+	public float speed = 10.0f; 
+	public float degrees = 60.0f; 
+	private float inRadians;
+	private bool touchingFloor;
+
+	void Start () {
+
+
+		inRadians = degrees * Mathf.Deg2Rad;
+		Cardboard.SDK.OnTrigger += PullTrigger;
+		cardhead = FindObjectOfType<CardboardHead> ();
+		rb = GetComponent<Rigidbody> ();
+		print (cardhead.Gaze);
+
+	}
+
+	void PullTrigger(){
+
+		if (touchingFloor) {
+			
+			Vector3 projectedVector = Vector3.ProjectOnPlane (cardhead.Gaze.direction, Vector3.up);
+			Vector3 jumpVector = Vector3.RotateTowards (projectedVector, Vector3.up, inRadians, 0);
+			rb.velocity = jumpVector * speed;
+
+		}
+
+
+	}
+
+	void OnCollisionEnter(Collision collision){
+
+		touchingFloor = true;
+
+	}
+
+	void OnCollisionExit(Collision collision){
+
+		touchingFloor = false;
+
+	}
+		
+	// Update is called once per frame
+	void Update () {
+	
+	
+		print (touchingFloor);
+	}
+}
